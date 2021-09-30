2de9ad9aa49a82005a93bad617e0d9b2dcefdf45
Daniel Mussie <dannymussie7@gmail.com>
2021-09-30 08:34:50 -0500

    Update joins-solution.sql

--- joins-solution.sql
+++ joins-solution.sql
@@ -7,5 +7,13 @@ JOIN "addresses" ON "customers"."id" = "addresses"."customer_id";
 
 SELECT * FROM "orders" 
 JOIN "line_items" ON "orders"."id" = "line_items"."order_id"
-JOIN "products" ON "line_items"."product_id" = "products"."description"; 
-??? Made my best attempt. Can't seem to get this one up and running 
+JOIN "products" ON "products"."id" = "line_items"."product_id";
+
+
+-- 5. Get the number of orders for each customer. 
+
+SELECT "c"."id", "c"."first_name", COUNT("o"."id") FROM "customers" as "c"
+JOIN "addresses" as "a" ON "c"."id" = "a"."customer_id"
+JOIN "orders" as "o" ON "a"."id" = "o"."address_id"
+GROUP BY "c"."id", "c"."first_name"
+LIMIT 100; 
